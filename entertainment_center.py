d35b9f14ff3f39d74fefac4d2f0bb8575fe5f792
Michelle Thomas <michelle.q.thomas@gmail.com>
2015-03-16 10:11:28 -0700

    Fixing formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -16,25 +16,25 @@ essm = media.Movie("Eternal Sunshine of the Spotless Mind",
       "https://www.youtube.com/watch?v=yE-f1alkq9I")
 
 amelie = media.Movie("Amelie",
-				"It tells the story of a shy waitress who decides to change the lives" \
+        "It tells the story of a shy waitress who decides to change the lives" \
           " of those around her for the better.",
-				"http://ia.media-imdb.com/images/M/MV5BMTYzNjkxMTczOF5BMl5BanBnXkFtZ" \
+        "http://ia.media-imdb.com/images/M/MV5BMTYzNjkxMTczOF5BMl5BanBnXkFtZ" \
           "TgwODg5NDc2MjE@._V1_SY317_CR0,0,214,317_AL_.jpg",
-				"https://www.youtube.com/watch?v=B-uxeZaM-VM")
+        "https://www.youtube.com/watch?v=B-uxeZaM-VM")
 
 tenenbaums = media.Movie("The Royal Tenenbaums",
-					"It follows the lives of three gifted siblings who experience great" \
+          "It follows the lives of three gifted siblings who experience great" \
             " success in youth, and even greater disappointment and failure " \
             "after their eccentric father leaves them in their adolescent " \
             "years.",
-					"http://upload.wikimedia.org/wikipedia/en/3/3b/The_Tenenbaums.jpg",
-					"https://www.youtube.com/watch?v=8Eg6yIwP2vs")
+          "http://upload.wikimedia.org/wikipedia/en/3/3b/The_Tenenbaums.jpg",
+          "https://www.youtube.com/watch?v=8Eg6yIwP2vs")
 
 godfather = media.Movie("The Godfather",
-					"Traces the problems of Michael Corleone in 1958 and a young" \
+          "Traces the problems of Michael Corleone in 1958 and a young" \
             " immigrant Vito Corleone in 1917's Hell's Kitchen.",
-					"http://upload.wikimedia.org/wikipedia/en/0/03/Godfather_part_ii.jpg",
-					"https://www.youtube.com/watch?v=qJr92K_hKl0")
+          "http://upload.wikimedia.org/wikipedia/en/0/03/Godfather_part_ii.jpg",
+          "https://www.youtube.com/watch?v=qJr92K_hKl0")
 
 movie_list = [essm, amelie, tenenbaums, godfather]
 fresh_tomatoes.open_movies_page(movie_list)
